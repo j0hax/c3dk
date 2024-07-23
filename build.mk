641c2be5c8c0fb6d8cfafc869d6f7df2bdc8d9e0
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2024-07-23 16:57:59 +0200

    Reorganize project

--- build.mk
+++ build.mk
@@ -7,13 +7,13 @@ DOCKER_TAG  ?= c3dk
 DOCKER      ?= $(DOCKER_CMD) run -it --rm -v $(CWD):$(CWD) -v $(SDK):$(SDK) \
                -w $(CWD) $(DOCKER_TAG)
 TOOLCHAIN   ?= $(DOCKER) riscv-none-elf
-SRCS        ?= $(SDK)/boot.c $(SOURCES)
+SRCS        ?= $(SDK)/src/boot.c $(SOURCES)
 CFLAGS      ?= -W -Wall -Wextra -Werror -Wundef -Wshadow -pedantic \
                -Wdouble-promotion -fno-common -Wconversion -std=gnu17 \
                -fno-builtin -march=rv32imc_zicsr -mabi=ilp32 -O2 \
-               -ffunction-sections -fdata-sections -I. -I$(SDK)/ \
+               -ffunction-sections -fdata-sections -I. -I$(SDK)/src/ \
                $(EXTRA_CFLAGS)
-LINKFLAGS   ?= -T$(SDK)/link.ld -nostdlib -nostartfiles \
+LINKFLAGS   ?= -T$(SDK)/src/link.ld -nostdlib -nostartfiles \
                -Wl,--gc-sections $(EXTRA_LINKFLAGS)
 
 # Default target
